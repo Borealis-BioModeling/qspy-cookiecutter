fad1b6ab0d857424f1eef130b49243276a915537
Blake A. Wilson <blakeaw@users.noreply.github.com>
2025-06-25 17:40:56 -0500

    Update config.py

--- {{ cookiecutter.module_name }}/config.py
+++ {{ cookiecutter.module_name }}/config.py
@@ -1,4 +1,16 @@
-import os
+from pathlib import Path
 
-SUMMARY_DIR = os.path.abspath("../model-summary")
-LOG_PATH = os.path.abspath("../.qspy/logs/qspy.log"")
+# Paths
+PROJ_ROOT = Path(__file__).resolve().parents[1]
+
+SUMMARY_DIR = PROJ_ROOT / "model-summary"
+LOG_PATH = PROJ_ROOT / ".qspy/logs/qspy.log"
+
+DATA_DIR = PROJ_ROOT / "data"
+RAW_DATA_DIR = DATA_DIR / "raw"
+INTERIM_DATA_DIR = DATA_DIR / "interim"
+PROCESSED_DATA_DIR = DATA_DIR / "processed"
+EXTERNAL_DATA_DIR = DATA_DIR / "external"
+
+REPORTS_DIR = PROJ_ROOT / "reports"
+FIGURES_DIR = REPORTS_DIR / "figures"
